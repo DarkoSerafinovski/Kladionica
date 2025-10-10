1b116d1ac9100bc6c2222ac5064f5df5c36f2e76
DarkoSerafinovski <darkoserafinovski@gmail.com>
2025-10-10 14:14:54 +0200

    Dodati javadoc komentari u sistemske operacije za pretragu objekata (SOPretraziKorisnika, SOPretraziTiket)

--- server/src/main/java/so/SOPretraziKorisnik.java
+++ server/src/main/java/so/SOPretraziKorisnik.java
@@ -8,17 +8,34 @@ import baza.DBBroker;
 import domen.Korisnik;
 
 /**
- *
+ * Sistemska operacija koja omogucava pretragu korisnika u bazi podataka
+ * na osnovu prosledjenog ID-a. Ako korisnik ne postoji, baca se izuzetak.
+ * 
  * @author Darko
  */
+
 public class SOPretraziKorisnik extends AbstractSO{
     
     private Korisnik rezultat;
 
+    /**
+     * Vraca korisnika koji je pronadjen tokom pretrage.
+     * 
+     * @return objekat klase {@link Korisnik} koji je pronadjen u bazi
+     */
+    
     public Korisnik getRezultat() {
         return rezultat;
     }
 
+    /**
+     * Izvrsava pretragu korisnika u bazi podataka.
+     * Ako korisnik sa zadatim ID-em ne postoji, baca se izuzetak.
+     * 
+     * @param parametar objekat klase {@link Korisnik} sa postavljenim ID-em
+     * @throws Exception ako korisnik ne postoji u bazi ili dodje do greske pri pretrazi
+     */
+    
     @Override
     protected void izvrsiOperaciju(Object parametar) throws Exception {
         Korisnik k = (Korisnik) parametar;
@@ -28,6 +45,12 @@ public class SOPretraziKorisnik extends AbstractSO{
         }
     }
 
+    /**
+     * Proverava da li je prosledjen validan korisnik za pretragu.
+     * 
+     * @param parametar objekat klase {@link Korisnik} koji se pretrazuje
+     * @throws Exception ako korisnik nije prosledjen ili ID nije validan
+     */
     @Override
     protected void preduslov(Object parametar) throws Exception {
         Korisnik k = (Korisnik) parametar;
--- server/src/main/java/so/SOPretraziTiket.java
+++ server/src/main/java/so/SOPretraziTiket.java
@@ -8,21 +8,42 @@ import baza.DBBroker;
 import domen.Tiket;
 
 /**
- *
+ * Sistemska operacija koja omogucava pretragu tiketa u bazi podataka
+ * na osnovu prosledjenog ID-a. Ako tiket ne postoji, baca se izuzetak.
+ * 
  * @author Darko
  */
+
 public class SOPretraziTiket extends AbstractSO{
 
     private Tiket rezultat;
 
+    /**
+     * Vraca tiket koji je pronadjen tokom pretrage.
+     * 
+     * @return objekat klase {@link Tiket} koji je pronadjen u bazi
+     */
+    
     public Tiket getRezultat() {
         return rezultat;
     }
 
+    /**
+     * Postavlja rezultat pretrage na zadati tiket.
+     * 
+     * @param rezultat objekat klase {@link Tiket} koji se postavlja kao rezultat
+     */
+    
     public void setRezultat(Tiket rezultat) {
         this.rezultat = rezultat;
     }
     
+    /**
+     * Proverava da li je prosledjen validan tiket za pretragu.
+     * 
+     * @param parametar objekat klase {@link Tiket} koji se pretrazuje
+     * @throws Exception ako tiket nije prosledjen ili ID nije validan (Manji je od 1)
+     */
     
     @Override
     protected void preduslov(Object parametar) throws Exception {
@@ -32,6 +53,14 @@ public class SOPretraziTiket extends AbstractSO{
         }
     }
 
+    /**
+     * Izvrsava pretragu tiketa u bazi podataka.
+     * Ako tiket sa zadatim ID-em ne postoji, baca se izuzetak.
+     * 
+     * @param parametar objekat klase {@link Tiket} sa postavljenim ID-em
+     * @throws Exception ako tiket ne postoji u bazi ili dodje do greske pri pretrazi
+     */
+    
     @Override
     protected void izvrsiOperaciju(Object parametar) throws Exception {
         Tiket t = (Tiket) parametar;
