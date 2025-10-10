fc177a6558ac69b7f0da74b29b743eff7d0db0f3
DarkoSerafinovski <darkoserafinovski@gmail.com>
2025-10-10 13:43:23 +0200

    Dodati javadoc komentari u sistemske operacije za kreiranje praznih objekata (SOKreirajKorisnika, SOKreirajTiket)

--- server/src/main/java/so/SOKreirajKorisnika.java
+++ server/src/main/java/so/SOKreirajKorisnika.java
@@ -8,21 +8,44 @@ import baza.DBBroker;
 import domen.Korisnik;
 
 /**
- *
+ * Sistemska operacija koja kreira novog korisnika u bazi podataka.
+ * Nakon uspesnog kreiranja, cuva se generisani ID korisnika.
+ * 
  * @author Darko
  */
+
 public class SOKreirajKorisnika extends AbstractSO {
 
     private Korisnik korisnik = new Korisnik();
 
+    /**
+     * Vraca korisnika koji je kreiran tokom izvrsavanja operacije.
+     * 
+     * @return objekat klase {@link Korisnik} koji je kreiran
+     */
+    
     public Korisnik getKorisnik() {
         return korisnik;
     }
 
+    /**
+     * Ova operacija ne zahteva posebne preduslove.
+     * 
+     * @param parametar objekat operacije (nije potreban)
+     * @throws Exception ako dodje do greske prilikom provere preduslova
+     */
+    
     @Override
     protected void preduslov(Object parametar) throws Exception {
     }
 
+    /**
+     * Izvrsava kreiranje novog korisnika u bazi podataka.
+     * Dobijeni ID se postavlja u lokalni objekat korisnika.
+     * 
+     * @param parametar objekat klase {@link Korisnik} (nije potreban u ovoj implementaciji)
+     * @throws Exception ako dodje do greske prilikom upisa u bazu
+     */
     @Override
     protected void izvrsiOperaciju(Object parametar) throws Exception {
         
--- server/src/main/java/so/SOKreirajTiket.java
+++ server/src/main/java/so/SOKreirajTiket.java
@@ -8,22 +8,46 @@ import baza.DBBroker;
 import domen.Tiket;
 
 /**
- *
+ * Sistemska operacija koja kreira novi tiket u bazi podataka.
+ * Pre kreiranja postavlja pocetne vrednosti za ulog, status, moguci dobitak i kvotu.
+ * 
  * @author Darko
  */
+
 public class SOKreirajTiket extends AbstractSO {
 
     private Tiket tiket;
 
+    /**
+     * Vraca tiket koji je kreiran tokom izvrsavanja operacije.
+     * 
+     * @return objekat klase {@link Tiket} koji je kreiran
+     */
+    
     public Tiket getTiket() {
         return tiket;
     }
 
+    /**
+     * Ova operacija ne zahteva posebne preduslove.
+     * 
+     * @param parametar objekat operacije (nije potreban)
+     * @throws Exception ako dodje do greske prilikom provere preduslova ukoliko se on kasnije doda
+     */
+    
     @Override
     protected void preduslov(Object parametar) throws Exception {
         
     }
 
+    /**
+     * Izvrsava kreiranje novog tiketa u bazi podataka.
+     * Postavlja pocetne vrednosti atributa i dodeljuje generisani ID.
+     * 
+     * @param parametar objekat klase {@link Tiket} koji se kreira
+     * @throws Exception ako dodje do greske prilikom upisa u bazu
+     */
+    
     @Override
     protected void izvrsiOperaciju(Object parametar) throws Exception {
         Tiket t = (Tiket) parametar;
