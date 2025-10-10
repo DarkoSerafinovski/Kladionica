de26944372cf64b1f520e384639eac1c6f800466
DarkoSerafinovski <darkoserafinovski@gmail.com>
2025-10-10 14:41:41 +0200

    Dodati javadoc komentari u sistemsku operaciju za prijavu radnika (SOPrijaviRadnika)

--- server/src/main/java/so/SOPrijavaRadnika.java
+++ server/src/main/java/so/SOPrijavaRadnika.java
@@ -8,16 +8,31 @@ import baza.DBBroker;
 import domen.Radnik;
 
 /**
- *
+ * Sistemska operacija koja omogucava prijavu radnika u sistem.
+ * Proverava validnost korisnickog imena i lozinke, a zatim ucitava podatke radnika iz baze.
+ * 
  * @author Darko
  */
 public class SOPrijavaRadnika extends AbstractSO{
 
     private Radnik radnik;
     
+    /**
+     * Vraca radnika koji je uspesno prijavljen.
+     * 
+     * @return objekat klase {@link Radnik} koji je prijavljen
+     */
+    
     public Radnik getRadnik(){
         return radnik;
     }
+    
+    /**
+     * Izvrsava prijavu radnika tako sto ucitava njegove podatke iz baze na osnovu korisnickog imena i lozinke.
+     * 
+     * @param parametar objekat klase {@link Radnik} sa korisnickim imenom i lozinkom
+     * @throws Exception ako dodje do greske prilikom ucitavanja iz baze
+     */
     
     @Override
     protected void izvrsiOperaciju(Object parametar) throws Exception{
@@ -27,6 +42,15 @@ public class SOPrijavaRadnika extends AbstractSO{
         radnik = r1;
     }
 
+    /**
+     * Proverava preduslove prijave radnika.
+     * Baca izuzetak ako objekat nije klase {@link Radnik}, ili ako korisnicko ime
+     * ili lozinka nisu uneti ili lozinka nije dovoljno duga.
+     * 
+     * @param parametar objekat klase {@link Radnik} koji se prijavljuje
+     * @throws Exception ako preduslovi nisu ispunjeni
+     */
+    
     @Override
     protected void preduslov(Object parametar) throws Exception {
         if (!(parametar instanceof Radnik)) {
