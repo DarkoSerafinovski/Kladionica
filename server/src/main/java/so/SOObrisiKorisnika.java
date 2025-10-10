68a1b87cf91fe5e352a12fdfcfc379618eae7ca5
DarkoSerafinovski <darkoserafinovski@gmail.com>
2025-10-10 14:05:45 +0200

    Dodati javadoc komentari u sistemsku operaciju za brisanje objekta (SOObrisiKorisnik)

--- server/src/main/java/so/SOObrisiKorisnika.java
+++ server/src/main/java/so/SOObrisiKorisnika.java
@@ -8,10 +8,21 @@ import baza.DBBroker;
 import domen.Korisnik;
 
 /**
- *
+ * Sistemska operacija koja brise korisnika iz baze podataka.
+ * Pre brisanja proverava se da li je prosledjen validan objekat korisnika.
+ * 
  * @author Darko
  */
+
 public class SOObrisiKorisnika extends AbstractSO{
+    
+    /**
+     * Proverava da li je prosledjen validan korisnik za brisanje.
+     * Baca izuzetak ako objekat nije klase {@link Korisnik} ili ako ID nije ispravan (Nije veci od 0).
+     * 
+     * @param parametar objekat klase {@link Korisnik} koji se brise
+     * @throws Exception ako korisnik nije validan ili nema validan ID
+     */
     
     @Override
     protected void preduslov(Object parametar) throws Exception {
@@ -23,7 +34,14 @@ public class SOObrisiKorisnika extends AbstractSO{
             throw new Exception("Korisnik nema validan ID.");
         }
     }
-
+    
+    /**
+     * Izvrsava brisanje korisnika iz baze podataka.
+     * 
+     * @param parametar objekat klase {@link Korisnik} koji se brise
+     * @throws Exception ako dodje do greske prilikom komunikacije sa bazom podataka
+     */
+    
     @Override
     protected void izvrsiOperaciju(Object parametar) throws Exception {
         Korisnik k = (Korisnik) parametar;
