a89c6e14afc9055b94e3ac30273ed41e8eca7e3f
DarkoSerafinovski <darkoserafinovski@gmail.com>
2025-10-10 14:47:39 +0200

    Dodati javadoc komentari u sistemsku operaciju za kreiranje popunjenog objekta (SOUbaciPoslovnicu)

--- server/src/main/java/so/SOUbaciPoslovnica.java
+++ server/src/main/java/so/SOUbaciPoslovnica.java
@@ -8,10 +8,22 @@ import baza.DBBroker;
 import domen.Poslovnica;
 
 /**
- *
+ * Sistemska operacija koja ubacuje novu poslovnicu u bazu podataka.
+ * Pre upisa proverava da li su svi obavezni podaci o poslovnici uneti.
+ * 
  * @author Darko
  */
+
 public class SOUbaciPoslovnica extends AbstractSO{
+    
+    /**
+     * Proverava da li je prosledjeni objekat validan za ubacivanje poslovnice.
+     * Baca izuzetak ako objekat nije klase {@link Poslovnica} ili ako neki obavezni
+     * podatak nije popunjen.
+     * 
+     * @param parametar objekat klase {@link Poslovnica} koji se ubacuje
+     * @throws Exception ako parametar nije validan ili nedostaju obavezni podaci
+     */
     
     @Override
     protected void preduslov(Object parametar) throws Exception {
@@ -28,6 +40,12 @@ public class SOUbaciPoslovnica extends AbstractSO{
         }
     }
 
+    /**
+     * Izvrsava ubacivanje poslovnice u bazu podataka.
+     * 
+     * @param parametar objekat klase {@link Poslovnica} koji se ubacuje
+     * @throws Exception ako dodje do greske prilikom upisa u bazu
+     */
     @Override
     protected void izvrsiOperaciju(Object parametar) throws Exception {
         Poslovnica p = (Poslovnica) parametar;
